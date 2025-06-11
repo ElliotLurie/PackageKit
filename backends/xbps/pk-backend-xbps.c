0236bfa9ce507406a78c3abfb166d4beb94b9c39
Elliot <ElliotLurie@mailo.com>
2025-06-11 19:46:24 -0400

    Fix filters again and name handling

--- backends/xbps/pk-backend-xbps.c
+++ backends/xbps/pk-backend-xbps.c
@@ -86,39 +86,44 @@ format_repo (const gchar *repository)
 	return repo + 1; /* Skip the "/" */
 }
 
-struct getpkg_data {
+struct query_data {
 	PkBitfield filters;
+	_Bool (*filter_func)(xbps_dictionary_t pkg, const char *name, void *data);
+	void *filter_data;
+
 	PkInfoEnum info;
 	PkBackendJob *job;
+
 	GSList *prev_pkgs;
 	const gchar *repo;
 };
 
 
 static int
-getpkg_cb (struct xbps_handle *xbps, xbps_object_t obj, const char *key, void *data, _Bool *done)
+query_packages_cb (struct xbps_handle *xbps, xbps_object_t obj, const char *key, void *data, _Bool *done)
 {
-	struct getpkg_data *gp = (struct getpkg_data *) data;
+	struct query_data *qd = (struct query_data *) data;
 	xbps_dictionary_t pkg = (xbps_dictionary_t) obj;
-	gchar *id, *name, *version;
+	gchar *id;
 
 	/* Package properties */
-	const gchar *arch, *repository = gp->repo, *short_desc;
+	const gchar *arch, *name = key, *pkgver, *repository = qd->repo, *short_desc, *version;
 
+	/* Filter packages */
 	xbps_dictionary_get_cstring_nocopy (pkg, "architecture", &arch);
-	if (pk_bitfield_contain_priority (gp->filters, PK_FILTER_ENUM_ARCH, -1) > 0
+	if (pk_bitfield_contain_priority (qd->filters, PK_FILTER_ENUM_ARCH, -1) > 0
 			&& g_strcmp0 (xbps->native_arch + 1, arch) != 0)
 		return 0;
 
-	if (pk_bitfield_contain_priority (gp->filters, PK_FILTER_ENUM_NOT_ARCH, -1) > 0
+	if (pk_bitfield_contain_priority (qd->filters, PK_FILTER_ENUM_NOT_ARCH, -1) > 0
 			&& g_strcmp0 (xbps->native_arch + 1, arch) == 0)
 		return 0;
 	
-	/* Parse name and version from pkgver */
-	xbps_dictionary_get_cstring (pkg, "pkgver", &name);
-	version = strrchr (name, '-');
-	version[0] = '\0';
-	version++;
+	if (qd->filter_func != NULL && !qd->filter_func (pkg, name, qd->filter_data))
+		return 0;
+
+	xbps_dictionary_get_cstring_nocopy (pkg, "pkgver", &pkgver);
+	version = xbps_pkg_version (pkgver);
 
 	if (repository == NULL) {
 		xbps_dictionary_get_cstring_nocopy (pkg, "repository", &repository);
@@ -126,9 +131,8 @@ getpkg_cb (struct xbps_handle *xbps, xbps_object_t obj, const char *key, void *d
 	}
 
 	id = pk_package_id_build (name, version, arch, repository);
-	g_free (name);
 
-	for (GSList *prev_pkg = gp->prev_pkgs; prev_pkg != NULL; prev_pkg = prev_pkg->next) {
+	for (GSList *prev_pkg = qd->prev_pkgs; prev_pkg != NULL; prev_pkg = prev_pkg->next) {
 		gchar *prev_id = (gchar *) prev_pkg->data;
 
 		if (g_strcmp0 (id, prev_id) == 0) {
@@ -137,65 +141,87 @@ getpkg_cb (struct xbps_handle *xbps, xbps_object_t obj, const char *key, void *d
 		}
 	}
 
-	gp->prev_pkgs = g_slist_append (gp->prev_pkgs, id);
+	qd->prev_pkgs = g_slist_append (qd->prev_pkgs, id);
 	xbps_dictionary_get_cstring_nocopy (pkg, "short_desc", &short_desc);
 
-	pk_backend_job_package (gp->job, gp->info, id, short_desc);
+	pk_backend_job_package (qd->job, qd->info, id, short_desc);
 	return 0;
 }
 
 static int
-getpkg_repo_cb (struct xbps_repo *repo, void *data, _Bool *done)
+query_repos_cb (struct xbps_repo *repo, void *data, _Bool *done)
 {
-	struct getpkg_data *gp = (struct getpkg_data *) data;
+	struct query_data *qd = (struct query_data *) data;
 	xbps_array_t keys;
 	int ret;
 
 	if (repo->idx == NULL) return 0;
-	gp->repo = format_repo (repo->uri);
+	qd->repo = format_repo (repo->uri);
 	
 	keys = xbps_dictionary_all_keys (repo->idx);
 
-	ret = xbps_array_foreach_cb (repo->xhp, keys, repo->idx, getpkg_cb, data);
+	ret = xbps_array_foreach_cb (repo->xhp, keys, repo->idx, query_packages_cb, data);
 	xbps_object_release (keys);
 	return ret;
+}
+
+static void
+query_packages (PkBackend *backend, PkBackendJob *job, struct query_data *qd, PkBitfield filters)
+{
+	struct xbps_handle *xbps = (struct xbps_handle *) pk_backend_get_user_data (backend);
+
+	qd->filters = filters;
+	qd->job = job;
+	qd->repo = NULL;
+	qd->prev_pkgs = NULL;
+
+	pk_backend_job_set_status (job, PK_STATUS_ENUM_QUERY);
+
+	if (filters == PK_FILTER_ENUM_UNKNOWN || pk_bitfield_contain_priority (filters, PK_FILTER_ENUM_INSTALLED, -1) >= 0) {
+		qd->info = PK_INFO_ENUM_INSTALLED;
+		xbps_pkgdb_foreach_cb (xbps, query_packages_cb, qd);
+	}
+
+	if (filters == PK_FILTER_ENUM_UNKNOWN || pk_bitfield_contain_priority (filters, PK_FILTER_ENUM_NOT_INSTALLED, -1) >= 0) {
+		qd->info = PK_INFO_ENUM_AVAILABLE;
+		xbps_rpool_foreach (xbps, query_repos_cb, qd);
+	}
+
+	g_slist_free_full (qd->prev_pkgs, g_free);
 }
 
 void
 pk_backend_get_packages (PkBackend *backend, PkBackendJob *job, PkBitfield filters)
 {
-	struct xbps_handle *xbps = (struct xbps_handle *) pk_backend_get_user_data (backend);
-	struct getpkg_data data;
+	struct query_data qd;
+	qd.filter_func = NULL;
+	qd.filter_data = NULL;
 
-	data.filters = filters;
-	data.job = job;
-	data.repo = NULL;
-	data.prev_pkgs = NULL;
-
-	pk_backend_job_set_status (job, PK_STATUS_ENUM_REQUEST);
-
-	if (pk_bitfield_contain_priority (filters, PK_FILTER_ENUM_INSTALLED, -1)) {
-		data.info = PK_INFO_ENUM_INSTALLED;
-		xbps_pkgdb_foreach_cb (xbps, getpkg_cb, &data);
-	}
-
-	if (pk_bitfield_contain_priority (filters, PK_FILTER_ENUM_NOT_INSTALLED, -1)) {
-		data.info = PK_INFO_ENUM_AVAILABLE;
-		xbps_rpool_foreach (xbps, getpkg_repo_cb, &data);
-	}
-
-	g_slist_free_full (data.prev_pkgs, g_free);
+	query_packages (backend, job, &qd, filters);
 	pk_backend_job_finished (job);
 }
 
-void pk_backend_search_names (PkBackend *backend, PkBackendJob *job, PkBitfield filters, gchar **values)
+static _Bool
+search_names_filter (xbps_dictionary_t pkg, const char *name, void *data)
 {
-	pk_backend_job_set_status (job, PK_STATUS_ENUM_QUERY);
-	
-	if (pk_bitfield_contain_priority (filters, PK_FILTER_ENUM_INSTALLED, -1)) {
+	gchar **values = (gchar **) data;
 
+	for (guint i = 0; i < g_strv_length (values); i++) {
+		if (strstr (name, values[i]))
+			return true;
 	}
 
+	return false;
+}
+
+void
+pk_backend_search_names (PkBackend *backend, PkBackendJob *job, PkBitfield filters, gchar **values)
+{
+	struct query_data qd;
+	qd.filter_func = search_names_filter;
+	qd.filter_data = values;
+
+	query_packages (backend, job, &qd, filters);
 	pk_backend_job_finished (job);
 }
 

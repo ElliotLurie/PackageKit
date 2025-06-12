d96d058f452b621527f796d15c9a1a62df1eadf2
Elliot <ElliotLurie@mailo.com>
2025-06-12 09:51:03 -0400

    Order of operations

--- backends/xbps/pk-backend-xbps.c
+++ backends/xbps/pk-backend-xbps.c
@@ -130,8 +130,13 @@ query_packages_cb (struct xbps_handle *xbps, xbps_object_t obj, const char *key,
 		repository = format_repo (repository);
 	}
 
-	id = pk_package_id_build (name, version, arch, repository);
+	/* Don't show "available" packages if they are installed on the system */
+	if (!(pk_bitfield_contain_priority (qd->filters, PK_FILTER_ENUM_INSTALLED, -1) > 0) &&
+			(pk_bitfield_contain_priority (qd->filters, PK_FILTER_ENUM_NOT_INSTALLED, -1) > 0) &&
+			xbps_pkg_is_installed (xbps, pkgver))
+		return 0;
 
+	id = pk_package_id_build (name, version, arch, repository);
 	for (GSList *prev_pkg = qd->prev_pkgs; prev_pkg != NULL; prev_pkg = prev_pkg->next) {
 		gchar *prev_id = (gchar *) prev_pkg->data;
 
@@ -140,12 +145,6 @@ query_packages_cb (struct xbps_handle *xbps, xbps_object_t obj, const char *key,
 			return 0;
 		}
 	}
-
-	/* Don't show "available" packages if they are installed on the system */
-	if (!(pk_bitfield_contain_priority (qd->filters, PK_FILTER_ENUM_INSTALLED, -1) > 0) &&
-			(pk_bitfield_contain_priority (qd->filters, PK_FILTER_ENUM_NOT_INSTALLED, -1) > 0) &&
-			xbps_pkg_is_installed (xbps, pkgver))
-		return 0;
 
 	qd->prev_pkgs = g_slist_append (qd->prev_pkgs, id);
 	xbps_dictionary_get_cstring_nocopy (pkg, "short_desc", &short_desc);
